2737ee0b09fbf8ec9ac050795cac998775b8f446
LakeSuburbia <sandermangelschots@telenet.be>
2021-03-28 23:21:27 +0200

    Start writing Views-functions

--- Releases/releases/ReleasesApp/views.py
+++ Releases/releases/ReleasesApp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
+from django.db import IntegrityError
+from .models import *
+
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "releases/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(email, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "releases/register.html", {
+                "message": "Email address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "mail/register.html")
