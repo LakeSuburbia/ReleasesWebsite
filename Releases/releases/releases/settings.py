f0ac0e6e73ca2b49820f4ce83e2e3adb83d575f6
LakeSuburbia <sandermangelschots@telenet.be>
2021-03-29 20:28:07 +0200

    Update settings to make migration work on server

--- Releases/releases/releases/settings.py
+++ Releases/releases/releases/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,7 +80,7 @@ WSGI_APPLICATION = 'releases.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
