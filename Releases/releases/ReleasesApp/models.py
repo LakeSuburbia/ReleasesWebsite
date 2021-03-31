0060c99b8a03c4c504ecbce1785ca7911ea0d23e
Mattis Swannet <mattis.swannet@student.kdg.be>
2021-03-31 15:37:21 +0200

    overrides __str__ method in Release to title of release object (instead of generic ID)

--- Releases/releases/ReleasesApp/models.py
+++ Releases/releases/ReleasesApp/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator, MinValueValidator 
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models.fields import CharField
 from django.db.models.fields.related import ForeignKey
+
 
 # Create your models here.
 class User(AbstractUser):
     pass
 
+
 class Release(models.Model):
     release_date = models.DateField()
     title = CharField(max_length=100)
     artist = CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 
 class ReleaseScore(models.Model):
@@ -23,6 +28,5 @@ class ReleaseScore(models.Model):
 class ReleasePublicScore:
     release = models.ForeignKey('Release', on_delete=models.CASCADE)
     score = models.ForeignKey('ReleaseScore', on_delete=models.CASCADE)
-    #globalaveragescore = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
-    #globalhottestscore = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
-
+    # globalaveragescore = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    # globalhottestscore = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
