9b884538433fe2cd7a6b64ec685cb4540e2812d5
LakeSuburbia <sandermangelschots@telenet.be>
2021-04-03 12:26:09 +0200

    Sander Mangelschots

---
+++ Releases/releases/git.py
@@ -0,0 +1,15 @@
+import sys
+import os
+
+if len(sys.argv) < 2:
+    print("You should add a message!")
+else:
+    message="\""
+    for i  in range(1,len(sys.argv)):
+        message = message + sys.argv[i] + " "
+    message=message+"\""
+    os.system("git checkout -b sander")
+    os.system("git add .")
+    os.system('git commit -m '+message)
+    print("Committed with message:")
+    print(message)
