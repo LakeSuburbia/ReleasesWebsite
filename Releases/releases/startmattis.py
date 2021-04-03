93d38e6f7b2cf8734e48b7f031e87aa43be1a647
LakeSuburbia <sandermangelschots@telenet.be>
2021-04-03 13:36:52 +0200

    this is a test

--- Releases/releases/startmattis.py
+++ Releases/releases/startmattis.py
@@ -3,7 +3,7 @@ import os
 
 user = "mattis"
 
-os.system("git pull")
+os.system("git pull origin main")
 os.system("git checkout -b "+user)
 os.system("git add .")
 print("You are now on branch "+user)
