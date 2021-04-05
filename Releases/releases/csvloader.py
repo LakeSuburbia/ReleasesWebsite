0fdd77ca8326574facf170f3b8bb0906973b05e7
Mattis Swannet <mattis.swannet@student.kdg.be>
2021-04-06 00:09:56 +0200

    bulk uploader for data from legacy spreadsheet

--- Releases/releases/csvloader.py
+++ Releases/releases/csvloader.py
@@ -1,37 +1,51 @@
 import requests
 import pandas as pd
-import pathlib
+import sys, os
 
 # initialise dataframe with csv
-path = pathlib.Path("C://Users/matt/PycharmProjects/ReleasesWebsite/Releases/releases/ReleasesApp/resources/legacy_releases.csv")
-releases = pd.read_csv(path)
+csv = pd.read_csv(os.path.realpath("ReleasesApp/resources/legacy_releases.csv"))
 
-# set column names to first row
-releases.rename(columns=releases.iloc[0])
+# csv cleanup
+csv.rename(columns=csv.iloc[0])
+csv.columns = csv.columns.str.lower()
 
-# lower case column names
-releases.columns = releases.columns.str.lower()
+# releases
 
-# remove scores
-releases = releases[["date", "title", "artist"]]
-
-# remove rows without data in vital columns
+#releases cleanup
+releases = csv[["date", "title", "artist"]]
 releases = releases[releases["date"].notna()]
 releases = releases[releases["title"].notna()]
 releases = releases[releases["artist"].notna()]
-
-# reformat date
 releases["date"] = releases["date"].str.replace(r"^([\d]{1,2})-([\d)]{1,2})-(\d\d\d\d)$", "\\3-\\2-\\1", regex=True)
 
-for release in releases.itertuples():
+for index, release in enumerate(releases.itertuples()):
     # format the JSON data
     releaseJSON = {
         "release_date": release.date,
         "artist": release.artist,
         "title": release.title
     }
-    print(releaseJSON)
 
     # send the data out!
     req = requests.post('http://127.0.0.1:8000/restapi/releases/', releaseJSON, auth=('ADMIN', 'ADMIN'))
-    print(req)
+    print("release", index, "result:", req)
+
+#scores
+
+# scores cleanup
+scores = csv.drop(csv.columns[[0, 1, 3, 4]], axis=1)
+scores.dropna(axis=0, how='all', inplace=True)
+
+for release in scores.head(1).itertuples(index=False):
+    print(release)
+    release = release._asdict()
+    title = release.pop('title')
+    for index, user in enumerate(release):
+        if release[user] != "nan":
+            scoreJSON = {
+                "user": user,
+                "release": title,
+                "score": release[user]
+            }
+            req = requests.post('http://127.0.0.1:8000/restapi/scores/', scoreJSON, auth=('ADMIN', 'ADMIN'))
+            print("release", index, "result:", req)
